f05ac5a3904f201b7e2a8a77eaabe2911cb1c033
TheKingOfSpade <122304903+TheKingOfSpade@users.noreply.github.com>
2023-02-06 09:52:35 -0800

    initial commit

---
+++ DataTyping.js
@@ -0,0 +1,25 @@
+console.log(32);
+
+console.log(-3);
+console.log(0);
+console.log(3);
+
+console.log(3.14);
+console.log(100.045);
+
+console.log("hello world");
+console.log('hello world');
+console.log("hello world");
+
+console.log(14+2);
+console.log(14-2);
+console.log(14/2);
+console.log(14%2);
+console.log(15%10);
+
+console.log("My name"+"is Joe Mama.");
+console.log('My' + ' ' + 'age' = ' '+'is'+ ' '+ 15 +'.');
+console.log("Your age is\n17.");
+
+console.log(`15 divided by 10 = ${15/10} axcvzxcvx`);
+
