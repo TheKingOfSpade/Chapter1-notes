0056c3f109cc207b6148b9e42c4ce99266e05332
TheKingOfSpade <122304903+TheKingOfSpade@users.noreply.github.com>
2023-02-17 10:27:17 -0800

    Initial commit

---
+++ WhileLoops1.js
@@ -0,0 +1,25 @@
+const readLine = require(`readline-sync`);
+
+let x = 0;
+while (x<26){
+    console.log(x);
+    x++;
+}
+
+let y = -1;
+while (y<50){
+    y++;
+    console.log(y);
+}
+
+let z = -10;
+while (z<11) {
+    console.log(z);
+    z++;
+}
+
+let a = 0;
+while (a > -51) {
+    console.log(a);
+    a--;
+}
